7a135b88e24ed0e7c93e9bb63c77b29854b9168f
nTorres <ntorres12@icloud.com>
2022-08-01 06:43:33 -0500

    a

---
+++ precache-manifest.54d466fdc29a1b022fd8df506e63f60c.js
@@ -0,0 +1,142 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "66084eda32a2335aea498afcd8ddaa56",
+    "url": "/index.html"
+  },
+  {
+    "revision": "67aa27ea2d28d8a03c8b",
+    "url": "/static/css/12.8beb1efa.chunk.css"
+  },
+  {
+    "revision": "4ec415762deacb46a4a0",
+    "url": "/static/css/15.b8803beb.chunk.css"
+  },
+  {
+    "revision": "38e32cebda85967a68fc",
+    "url": "/static/css/19.b8803beb.chunk.css"
+  },
+  {
+    "revision": "f01625c2a5a2c18ed209",
+    "url": "/static/css/5.b8803beb.chunk.css"
+  },
+  {
+    "revision": "e773e2c104e67ee8634e",
+    "url": "/static/css/7.08bb1911.chunk.css"
+  },
+  {
+    "revision": "0a4024cbafb5c7166fc6",
+    "url": "/static/css/main.3cac3af4.chunk.css"
+  },
+  {
+    "revision": "9d70e31e035735642146",
+    "url": "/static/js/0.e53266a5.chunk.js"
+  },
+  {
+    "revision": "029d38c8988237133c1f",
+    "url": "/static/js/1.1caa9678.chunk.js"
+  },
+  {
+    "revision": "67aa27ea2d28d8a03c8b",
+    "url": "/static/js/12.8f5cecde.chunk.js"
+  },
+  {
+    "revision": "cc9ff2f3fb7c4d5ee4730e5127f733f3",
+    "url": "/static/js/12.8f5cecde.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2c5208eabb00e657d222",
+    "url": "/static/js/13.c83ff82e.chunk.js"
+  },
+  {
+    "revision": "e56f92b99fdcf7537ad5",
+    "url": "/static/js/14.2f351fa4.chunk.js"
+  },
+  {
+    "revision": "275fe79abee3b697f1673c8bd9c58856",
+    "url": "/static/js/14.2f351fa4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4ec415762deacb46a4a0",
+    "url": "/static/js/15.58d7cfe5.chunk.js"
+  },
+  {
+    "revision": "ae9f5776706b15a9460f",
+    "url": "/static/js/16.97cda96e.chunk.js"
+  },
+  {
+    "revision": "fe7ae5b23f551d3eecda",
+    "url": "/static/js/17.f34fa97e.chunk.js"
+  },
+  {
+    "revision": "c381bfd32e72416455d9",
+    "url": "/static/js/18.90717743.chunk.js"
+  },
+  {
+    "revision": "38e32cebda85967a68fc",
+    "url": "/static/js/19.19a74698.chunk.js"
+  },
+  {
+    "revision": "84d9645316c718a83602",
+    "url": "/static/js/2.abc235c9.chunk.js"
+  },
+  {
+    "revision": "81aca18fad7bcd9dcda5",
+    "url": "/static/js/20.765852d5.chunk.js"
+  },
+  {
+    "revision": "8f0fe44cc22686446dd3",
+    "url": "/static/js/21.6fab6c60.chunk.js"
+  },
+  {
+    "revision": "faa903730332fe2da517",
+    "url": "/static/js/22.2a8e6822.chunk.js"
+  },
+  {
+    "revision": "9a6488ea02e39e015f5d",
+    "url": "/static/js/23.874d8e2b.chunk.js"
+  },
+  {
+    "revision": "91a17b96f6ed6ef040e8",
+    "url": "/static/js/24.adbf91c9.chunk.js"
+  },
+  {
+    "revision": "78b9c159d66433774bb5",
+    "url": "/static/js/3.f42d2471.chunk.js"
+  },
+  {
+    "revision": "362cd232e6b400756e3bc1723069a8cd",
+    "url": "/static/js/3.f42d2471.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "130ff64affa6c2778ae2",
+    "url": "/static/js/4.9d8b635e.chunk.js"
+  },
+  {
+    "revision": "f01625c2a5a2c18ed209",
+    "url": "/static/js/5.b3343584.chunk.js"
+  },
+  {
+    "revision": "67d392815cc51363e45b",
+    "url": "/static/js/6.5afcd235.chunk.js"
+  },
+  {
+    "revision": "e773e2c104e67ee8634e",
+    "url": "/static/js/7.387806cf.chunk.js"
+  },
+  {
+    "revision": "3e4bb5df9bab7c7486fe",
+    "url": "/static/js/8.98b7666f.chunk.js"
+  },
+  {
+    "revision": "6fb629b32e87c2cdd4a4",
+    "url": "/static/js/9.7dd542b9.chunk.js"
+  },
+  {
+    "revision": "0a4024cbafb5c7166fc6",
+    "url": "/static/js/main.ef449ff6.chunk.js"
+  },
+  {
+    "revision": "75c8b63ca47d3cfdc24f",
+    "url": "/static/js/runtime-main.fcb0da6c.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.ea3171c1477b67a52516318a5e354bdd.js"
+  "/precache-manifest.54d466fdc29a1b022fd8df506e63f60c.js"
 );
 
 self.addEventListener('message', (event) => {
